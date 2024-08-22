1dfb69f292428ff296dc09cd127b43aa7eff8e85
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-08-22 08:35:41 -0400

    create content page template

--- assets/js/add-components.js
+++ assets/js/add-components.js
@@ -1,7 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('components/sidebar.html')
+    let basePath = '';
+    const currentPath = window.location.pathname;
+    const depth = currentPath.split('/').length - 2; // Adjust based on your directory structure
+
+    for (let i = 0; i < depth; i++) {
+        basePath += '../';
+    }
+
+    // Load sidebar
+    fetch(basePath + 'components/sidebar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('sidebar-container').innerHTML = data;
+    });
+
+    // Load header
+    fetch(basePath + 'components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+
+    // Load footer
+    fetch(basePath + 'components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
     });
 });
