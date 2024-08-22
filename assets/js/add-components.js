b196fa6017a710c1ead3a48d5e6979e4c8670a73
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-08-22 09:39:47 -0400

    fix paths

--- assets/js/add-components.js
+++ assets/js/add-components.js
@@ -1,28 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let basePath = '';
-    const currentPath = window.location.pathname;
-    const depth = currentPath.split('/').length - 2; // Adjust based on your directory structure
-
-    for (let i = 0; i < depth; i++) {
-        basePath += '../';
-    }
-
     // Load sidebar
-    fetch(basePath + 'components/sidebar.html')
+    fetch('../components/sidebar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('sidebar-container').innerHTML = data;
     });
 
     // Load header
-    fetch(basePath + 'components/header.html')
+    fetch('../components/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
     });
 
     // Load footer
-    fetch(basePath + 'components/footer.html')
+    fetch('../components/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
