460c7f919e090fcb5c997af7303c426057e6c462
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-08-27 09:42:42 -0400

    more demo content

---
+++ assets/js/add-components-checks.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Load sidebar
+    fetch('../components/sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('sidebar-container').innerHTML = data;
+    });
+
+    // Load header
+    fetch('../components/header-checks.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+        $('.selectpicker').selectpicker();
+    });
+
+    // Load footer
+    fetch('../components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-container').innerHTML = data;
+    });
+});
