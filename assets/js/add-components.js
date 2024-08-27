d877106dd1d449a275f053fc0f2b083ccf22e40a
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-08-26 22:22:09 -0400

    correct RM

--- assets/js/add-components.js
+++ assets/js/add-components.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
+        $('.selectpicker').selectpicker();
     });
 
     // Load footer
